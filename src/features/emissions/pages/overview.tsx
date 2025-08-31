06bef39d16bdde92c327f0b2f78d1a14590cd7c2
Shafeeq Ahamed S <shafeeq.ahamed@the6sconsulting.com>
2025-08-31 13:04:58 +0530

    Updated overview.tsx

--- src/features/emissions/pages/overview.tsx
+++ src/features/emissions/pages/overview.tsx
@@ -119,11 +119,9 @@ export function Overview() {
             </div>
 
             {/* Charts and Fleet Status */}
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="flex flex-col gap-6">
                 {/* Emission Trends Chart */}
-                <div className="lg:col-span-2">
-                    <EmissionTrendsChart />
-                </div>
+                <EmissionTrendsChart />
 
                 {/* Fleet Status */}
                 <Card>
